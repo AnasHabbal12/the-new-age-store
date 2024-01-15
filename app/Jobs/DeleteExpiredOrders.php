1970120ee92786f75971ae4f748f12d30c5a2f3e
unknown <appteco.web@hotmail.com>
2024-01-15 17:00:33 +0300

    15-1

---
+++ app/Jobs/DeleteExpiredOrders.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteExpiredOrders implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //Order::whereDate('created_at', '<', now()->subDays(3))
+        //->where('status', 'pending')->delete();
+        //Order::where('number', 20230001)->delete();
+        //Order::destroy();
+        //Product::where('id', 31)->delete();
+    }
+}
